eb7c8f6955bcb5817e35506413e2e583d39669d4
Asif Peerzade <Apeerzade785@gmail.com>
2025-07-01 18:51:26 +0000

    update the gemini model to this gemini-2.5-flash-lite-preview-06-17

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -24,5 +24,5 @@ if (!process.env.GOOGLE_API_KEY || process.env.GOOGLE_API_KEY.includes("YOUR_") 
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.5-pro-preview-06-05',
+  model: 'googleai/gemini-2.5-flash-lite-preview-06-17',
 });
