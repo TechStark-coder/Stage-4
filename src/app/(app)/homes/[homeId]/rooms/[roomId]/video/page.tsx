37c8ac6805816434b436738682d26ae210237943
Asif Peerzade <Apeerzade785@gmail.com>
2025-07-01 19:07:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/homes/[homeId]/rooms/[roomId]/video/page.tsx
+++ src/app/(app)/homes/[homeId]/rooms/[roomId]/video/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
 import { useAuthContext } from "@/hooks/useAuthContext";
